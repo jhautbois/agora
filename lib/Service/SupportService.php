d2610379e8ba88e511e9392921036a6fe658f8be
Jean-Michel Hautbois <jeanmichel.hautbois@yoseli.org>
2026-09-25 08:10:08 +0200

    fix(support): check access before listing an inquiry's votes
    Listing the votes of an inquiry now requires access to that inquiry.
    
    Signed-off-by: Jean-Michel Hautbois <jeanmichel.hautbois@yoseli.org>
    (cherry picked from commit b76278196b6dce144380895397a615395c5da0fd)

--- lib/Service/SupportService.php
+++ lib/Service/SupportService.php
@@ -893,6 +893,7 @@ public function list(int $inquiryId, bool $wRoles = true): array
  */
 public function getSupportByInquiryId(int $inquiryId): array
 {
+    $this->inquiryMapper->get($inquiryId, withRoles: true)->request(Inquiry::PERMISSION_INQUIRY_VIEW);
     return $this->hideOthersSupports($this->supportMapper->findByInquiryId($inquiryId), $inquiryId);
 }
 
